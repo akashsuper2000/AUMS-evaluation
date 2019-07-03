7eccd4985db54217b07e413d67fd84ec9d9bb821
Akash <akashsuper2000@gmail.com>
2019-07-03 23:06:29 +0530

    Ready one

--- index.js
+++ index.js
@@ -44,10 +44,5 @@ function onreq(req,res){
 
 app.post('/api', onreq);
 
-const port = process.env.PORT || 8080;
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+const port = process.env.PORT || 4000;
 app.listen(port);
