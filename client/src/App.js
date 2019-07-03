a9050745a126a72ea9dedd7cf02ce979c81a427a
Akash <akashsuper2000@gmail.com>
2019-07-03 22:20:16 +0530

    working

---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+// import axios from 'axios';
+import './Components/bootstrap.css';
+import './App.css';
+
+class App extends React.Component {
+   constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+  
+  // setStateHandler(event) {
+  //     var user = document.getElementById('user');
+  //     var pass = document.getElementById('pass');
+
+  //     const cred = {user, pass};
+
+  //     // axios
+  //     //   .post('http://localhost:4000/api', cred)
+  //     //   .then(() => console.log('Book Created'))
+  //     //   .catch(err => {console.error(err);});
+  // };
+
+  render() {
+    return (
+       <div style={{padding: 50}}>
+       <p className="App-intro">{this.state.apiResponse}</p>
+       <form method='POST' action='http://localhost:4000/api'>
+        <h1 style={{fontSize: 100}}>Sign In</h1>
+          <input style={{marginTop: 50, width: 500, borderColor: "black", borderWidth: 2, borderRadius: 5, padding: 10, display: 'block'}} type="text" name='user' id="user" placeholder="Username"/>
+          <input style={{marginTop: 50, width: 500, borderColor: "black", borderWidth: 2, borderRadius: 5, padding: 10, display: 'block'}} type="password" name='pass' id="pass" placeholder="Password"/>
+          <input style={{marginTop: 50, borderWidth: 2, borderColor: 'black'}} className="btn-lg btn btn-light" type="submit" value="Fill it up for me!"/>
+      </form>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const path = require('path');
+var http = require('http');
+var url = require('url');
+var bodyParser = require('body-parser');
+const { spawn } = require('child_process')
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+function onreq(req,res){
+
+	var user = req.body.user;
+	var pass = req.body.pass;
+    const process = spawn('python', ['./aumseval.py', user, pass]);
+
+ //    const out = []
+ //    process.stdout.on(
+ //      'data',
+ //      (data) => {
+ //        out.push(data.toString());
+ //        res.sendStatus(data);
+ //      }
+ //    );
+
+
+ //    const err = []
+ //    process.stderr.on(
+ //      'data',
+ //      (data) => {
+ //        err.push(data.toString());
+ //        res.sendStatus(data);
+ //      }
+ //    );
+
+ //    process.on('exit', (code, signal) => {
+ //      res.sendStatus(`${code} (${signal})`);
+ //      resolve(out);
+ //    });
+
+	// });
+}
+
+app.post('/api', onreq);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port);
