c76a7ae3dfd83adc9d69997edd5ac5ba7fee130c
Akash <akashsuper2000@gmail.com>
2019-07-04 09:41:35 +0530

    final

--- index.js
+++ index.js
@@ -9,43 +9,6 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-function onreq(req,res){
-
-	var user = req.body.user;
-	var pass = req.body.pass;
-    const process = spawn('python', ['./aumseval.py', user, pass]);
-
- //    const out = []
- //    process.stdout.on(
- //      'data',
- //      (data) => {
- //        out.push(data.toString());
- //        res.sendStatus(data);
- //      }
- //    );
-
-
- //    const err = []
- //    process.stderr.on(
- //      'data',
- //      (data) => {
- //        err.push(data.toString());
- //        res.sendStatus(data);
- //      }
- //    );
-
- //    process.on('exit', (code, signal) => {
- //      res.sendStatus(`${code} (${signal})`);
- //      resolve(out);
- //    });
-
-	// });
-}
-
-app.post('/api', onreq);
-
 const extendTimeoutMiddleware = (req, res, next) => {
   const space = ' ';
   let isFinished = false;
@@ -103,6 +66,41 @@ app.use(extendTimeoutMiddleware);
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
+
+function onreq(req,res){
+
+	var user = req.body.user;
+	var pass = req.body.pass;
+    const process = spawn('python', ['./aumseval.py', user, pass]);
+
+ //    const out = []
+ //    process.stdout.on(
+ //      'data',
+ //      (data) => {
+ //        out.push(data.toString());
+ //        res.sendStatus(data);
+ //      }
+ //    );
+
+
+ //    const err = []
+ //    process.stderr.on(
+ //      'data',
+ //      (data) => {
+ //        err.push(data.toString());
+ //        res.sendStatus(data);
+ //      }
+ //    );
+
+ //    process.on('exit', (code, signal) => {
+ //      res.sendStatus(`${code} (${signal})`);
+ //      resolve(out);
+ //    });
+
+	// });
+}
+
+app.post('/api', onreq);
 
 const port = process.env.PORT || 5000;
 app.listen(port);
