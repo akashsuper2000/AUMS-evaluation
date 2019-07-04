e52b049a2209e6bf616c6155baf3e09fb4b173a7
Akash <akashsuper2000@gmail.com>
2019-07-04 11:47:06 +0530

    initial commit

---
+++ client/build/precache-manifest.5b0520d34ac2b86a7bee9b6c5083465e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "520bd314fa44c55a3d68df92486d0c7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bd8ce13583303819499",
+    "url": "/static/css/main.d2adf8e2.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "5bd8ce13583303819499",
+    "url": "/static/js/main.ed9f6204.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
