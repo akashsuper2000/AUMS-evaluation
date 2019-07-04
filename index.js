61f4edb1e5fdf12075f8f5841edfb46163ca6f08
Akash <akashsuper2000@gmail.com>
2019-07-04 11:40:09 +0530

    initial commit

--- index.js
+++ index.js
@@ -9,91 +9,11 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-const extendTimeoutMiddleware = (req, res, next) => {
-  const space = ' ';
-  let isFinished = false;
-  let isDataSent = false;
-
-  // Only extend the timeout for API requests
-  if (!req.url.includes('/api')) {
-    next();
-    return;
-  }
-
-  res.once('finish', () => {
-    isFinished = true;
-  });
-
-  res.once('end', () => {
-    isFinished = true;
-  });
-
-  res.once('close', () => {
-    isFinished = true;
-  });
-
-  res.on('data', (data) => {
-    // Look for something other than our blank space to indicate that real
-    // data is now being sent back to the client.
-    if (data !== space) {
-      isDataSent = true;
-    }
-  });
-
-  const waitAndSend = () => {
-    setTimeout(() => {
-      // If the response hasn't finished and hasn't sent any data back....
-      if (!isFinished && !isDataSent) {
-        // Need to write the status code/headers if they haven't been sent yet.
-        if (!res.headersSent) {
-          res.writeHead(202);
-        }
-
-        res.write(space);
-
-        // Wait another 15 seconds
-        waitAndSend();
-      }
-    }, 15000);
-  };
-
-  waitAndSend();
-  next();
-};
-
-app.use(extendTimeoutMiddleware);
-
 function onreq(req,res){
 
 	var user = req.body.user;
 	var pass = req.body.pass;
     const process = spawn('python', ['./aumseval.py', user, pass]);
-
- //    const out = []
- //    process.stdout.on(
- //      'data',
- //      (data) => {
- //        out.push(data.toString());
- //        res.sendStatus(data);
- //      }
- //    );
-
-
- //    const err = []
- //    process.stderr.on(
- //      'data',
- //      (data) => {
- //        err.push(data.toString());
- //        res.sendStatus(data);
- //      }
- //    );
-
- //    process.on('exit', (code, signal) => {
- //      res.sendStatus(`${code} (${signal})`);
- //      resolve(out);
- //    });
-
-	// });
 }
 
 app.post('/api', onreq);
